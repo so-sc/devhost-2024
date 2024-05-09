be79b7d7c7184fb2299a1cde5efefcb3a7b6d6f2
srajan <kumarsrajan02@gmail.com>
2024-05-09 09:56:37 +0530

    coming soon done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,45 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import React from "react";
+import { InfiniteMovingCards } from "@/components/ui/infinite-moving-cards";
+
+const testimonials = [
+  {
+    title: "Coming Soon",
+  },
+  {
+    title: "Stay Tuned",
+  },
+  {
+    title: "Coming Soon",
+  },
+  {
+    title: "Stay Tuned",
+  },
+  {
+    title: "Coming Soon",
+  },
+  {
+    title: "Stay Tuned",
+  },
+];
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-[100dvh] px-10">
-      <div>
-        <Image
+      <div className="absolute -z-20 inset-0 overflow-hidden">
+        <img
           src="/bg2.svg"
-          className="absolute top-0 left-0 w-full -z-10"
-          alt="logo"
           width={500}
           height={500}
-        />
+          alt="background"
+          className="object-right lg:h-full md:h-[75%] h-[50%] w-full object-cover"
+        ></img>
       </div>
       <div className="flex flex-col pb-6 justify-center items-center">
         <div className="flex gap-5 justify-center items-center">
           <Image
             src="/sosc_logo.svg"
-            className="w-28 h-fit"
+            className="w-16 h-fit"
             alt="logo"
             width={500}
             height={500}
@@ -25,28 +47,31 @@ export default function Home() {
           <Image
             src="/niveus_logo_white.png"
             alt="logo"
-            className="w-32 h-fit"
+            className="w-20 h-fit"
             width={500}
             height={500}
           />
         </div>
-        <p className="pt-1 tracking-wide">Presents</p>
+        <p className="pt-2 tracking-wide">Presents</p>
       </div>
       <div className="flex w-fit flex-col">
         <Image
           src="/logo.svg"
           alt="logo"
-          className="w-80"
+          className="md:w-80 w-64"
           width={500}
           height={500}
         />
-        <p className="text-sm pt-3 tracking-wide text-primary">
+        <p className="md:text-sm text-xs pt-3 tracking-wide text-primary">
           An initiative towards the silicon beach.
         </p>
       </div>
-      <div className="flex flex-col pt-10 justify-center items-center">
-        {/* <Button>Register Now</Button> */}
-        <p className="text-xl">Coming Soon</p>
+      <div className="w-96 pt-5 md:w-auto md:max-w-3xl">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
+
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -67,14 +71,32 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
-} satisfies Config
+  plugins: [require("tailwindcss-animate"), addVariablesForColors],
+} satisfies Config;
 
-export default config
+export default config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
