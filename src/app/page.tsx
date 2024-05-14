1743ac01367f4712269096478e4d316160949009
srajan <kumarsrajan02@gmail.com>
2024-05-14 07:31:28 +0530

    add: cursor tail

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ import Speakers from "@/components/Speakers";
 import FAQ from "@/components/Faq";
 import Hero from "@/components/Hero";
 import Schedule from "@/components/Schedule";
+import CursorTrailCanvas from "@/components/CursorTrailCanvas";
 
 export default function Home() {
   return (
     <main className="max-w-6xl px-5 mx-auto">
+      <CursorTrailCanvas className="pointer-events-none z-50 md:flex hidden fixed inset-0 -z-10 h-full w-full" />
       <Hero />
       <Schedule />
       {/* <Speakers /> */}
---
+++ src/components/CursorTrailCanvas.tsx
@@ -0,0 +1,33 @@
+import { CSSProperties, useEffect, useRef } from "react";
+
+import { cursorTrail } from "@/utility/cursor-tail";
+
+export interface CursorTrailCanvasProps {
+  color?: string;
+  className?: string;
+  style?: CSSProperties;
+}
+
+export default function CursorTrailCanvas(props: CursorTrailCanvasProps) {
+  const refCanvas = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const { cleanUp, renderTrailCursor } = cursorTrail({
+      ref: refCanvas,
+      color: props.color,
+    });
+    renderTrailCursor();
+
+    return () => {
+      cleanUp();
+    };
+  }, [props.color]);
+
+  return (
+    <canvas
+      ref={refCanvas}
+      className={props.className}
+      style={props.style}
+    ></canvas>
+  );
+}
