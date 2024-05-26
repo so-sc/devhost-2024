0458d9c2b8de9faeb86132468b14db6e575e3b0f
srajan <kumarsrajan02@gmail.com>
2024-05-26 14:14:02 +0530

    animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,62 +29,102 @@ const testimonials = [
 export default function Hero() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <div className="flex flex-col pb-6 justify-center items-center">
-        <div className="flex gap-5 justify-center items-center">
-          <Image
-            priority
-            src="/sosc_logo.svg"
-            className="w-16 h-14"
-            alt="logo"
-            width={500}
-            height={500}
-          />
-          <Image
-            priority
-            src="/niveus_logo_white.png"
-            alt="logo"
-            className="w-20 h-4"
-            width={500}
-            height={500}
-          />
-        </div>
-        <p className="pt-2 tracking-wide">Presents</p>
-      </div>
-      <div className="flex w-fit flex-col">
-        <Image
-          priority
-          src="/logo.svg"
-          alt="logo"
-          className="md:w-80 md:h-[10.5rem] w-64 h-[8.4rem]"
-          width={500}
-          height={500}
-        />
-        <p className="md:text-sm text-xs pt-3 tracking-wide text-primary">
-          Where Curiosity Meets Expertise.
-        </p>
-      </div>
-      <div className="w-96 pt-5 md:w-auto md:max-w-3xl">
-        {/* <InfiniteMovingCards
-          items={testimonials}
-          direction="right"
-          speed="slow"
-        /> */}
-        <h1 className="text-2xl font-medium tracking-wide md:px-5 px-2 pb-20 md:pb-10 text-center">
-          14th - 16th June
-        </h1>
-      </div>
-      {/* <div className="pb-20 pt-5 md:pb-10">
-        <HoverBorderGradient
-          containerClassName="rounded-full"
-          as="button"
-          className="bg-background px-10 group flex items-center space-x-2"
-        >
-          <span>Register Now</span>
-          <span className="group-hover:text-primary">
-            <ArrowRightIcon />
-          </span>
-        </HoverBorderGradient>
-      </div> */}
+      <motion.div
+        initial={{ y: -10, opacity: 0, filter: "blur(5px)" }}
+        animate={{ y: 0, opacity: 1, filter: "blur(0px)" }}
+        transition={{
+          ease: "linear",
+          stiffness: 20,
+          damping: 10,
+          duration: 0.3,
+        }}
+      >
+        <AnimatePresence mode="wait">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.25 }}
+          >
+            <div className="flex flex-col pb-6 justify-center items-center">
+              <div className="flex gap-5 justify-center items-center">
+                <Image
+                  priority
+                  src="/sosc_logo.svg"
+                  className="w-16 h-14"
+                  alt="logo"
+                  width={500}
+                  height={500}
+                />
+                <Image
+                  priority
+                  src="/niveus_logo_white.png"
+                  alt="logo"
+                  className="w-20 h-4"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <p className="pt-2 tracking-wide">Presents</p>
+            </div>
+            <div className="flex w-fit flex-col">
+              <Image
+                priority
+                src="/logo.svg"
+                alt="logo"
+                className="md:w-80 md:h-[10.5rem] w-64 h-[8.4rem]"
+                width={500}
+                height={500}
+              />
+              <p className="md:text-sm text-xs pt-3 tracking-wide text-primary">
+                Where Curiosity Meets Expertise.
+              </p>
+            </div>
+          </motion.div>
+        </AnimatePresence>
+      </motion.div>
+      <motion.div
+        initial={{ y: 10, opacity: 0, filter: "blur(5px)" }}
+        animate={{ y: 0, opacity: 1, filter: "blur(0px)" }}
+        transition={{
+          ease: "linear",
+          stiffness: 20,
+          damping: 10,
+          duration: 0.3,
+        }}
+      >
+        <AnimatePresence mode="wait">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.1 }}
+          >
+            <div className="w-96 pt-5 md:w-auto md:max-w-3xl">
+              {/* <InfiniteMovingCards
+                items={testimonials}
+                direction="right"
+                speed="slow"
+              /> */}
+              <h1 className="text-2xl font-medium tracking-wide md:px-5 px-2 pb-20 md:pb-10 text-center">
+                14th - 16th June
+              </h1>
+            </div>
+            {/* <div className="pb-20 pt-5 md:pb-10">
+              <HoverBorderGradient
+                containerClassName="rounded-full"
+                as="button"
+                className="bg-background px-10 group flex items-center space-x-2"
+              >
+                <span>Register Now</span>
+                <span className="group-hover:text-primary">
+                  <ArrowRightIcon />
+                </span>
+              </HoverBorderGradient>
+            </div> */}
+          </motion.div>
+        </AnimatePresence>
+      </motion.div>
       <div className="absolute z-40 float1 bottom-0 py-10">
         <svg
           className="w-8"
