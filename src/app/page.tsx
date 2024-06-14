a638fe902778d3a7eb438dc186d5a01eeeb012ec
srajan <kumarsrajan02@gmail.com>
2024-06-14 07:25:10 +0530

    rm: counter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
             className="object-right lg:w-[53rem] lg:h-[53rem] md:w-[40rem] md:h-[40rem] w-[30rem] h-[30rem] md:mt-96 sm:mt-[30rem] mt-[40rem] object-cover"
           ></Image>
         </div>
-        <Counter />
+        {/* <Counter /> */}
         <Schedule />
         <Speakers />
       </div>
