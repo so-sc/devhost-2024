4e284e9f6cda8bd3f550e215d393d3863587ef02
kosh <kosh@kosh-web.cfd>
2024-08-29 17:00:27 +0530

    Centered about hackathon and about devhost

--- src/components/AboutHackathon.tsx
+++ src/components/AboutHackathon.tsx
@@ -14,14 +14,14 @@ export default function AboutHackathon() {
     };
   return (
     <div className="flex justify-center pb-10 items-center w-full">
-      <div className="max-w-6xl mb-20 w-full md:ml-20 event_card md:mt-10 border-white/10 border rounded-xl pt-10 pb-10">
+      <div className="max-w-6xl mb-20 w-full event_card md:mt-10 border-white/10 border rounded-xl pt-10 pb-10">
         <h1 className="select-none text-center text-3xl md:text-4xl font-semibold md:pb-0 pb-0">
             {hackathonData.title}
         </h1>
         <div className="grid md:grid-cols-1">
         <div className="md:relative flex flex-col">
-          <div className="md:ml-20 event_card md:mt-10 border-none rounded-xl">
-            <div className="ml-5 md:mt-5 md:ml-[6.5rem] my-5 md:mr-5 mr-5">
+          <div className="event_card md:mt-10 border-none rounded-xl">
+            <div className="ml-5 md:mt-5 md:ml-[6.5rem] my-5 md:mr-[6.5rem] mr-5">
               <p className="text-lg tracking-wider py-2 ">
               {hackathonData.about}
               </p>
